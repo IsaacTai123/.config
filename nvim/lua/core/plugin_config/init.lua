8685a78d4fb5a5387b576d4d217cb95c8fc3cc4c
Isaac T <kevincurry438@gmail.com>
2023-06-12 23:23:01 +0800

    Add init.lua in plugin_config/ to make the structure more organized (we require this inside the main init.lua)

---
+++ nvim/lua/core/plugin_config/init.lua
@@ -0,0 +1,6 @@
+require("core.plugin_config.gruvbox")
+require("core.plugin_config.lualine")
+require("core.plugin_config.nvim-tree")
+require("core.plugin_config.floaterm")
+require("core.plugin_config.bufferline")
+
